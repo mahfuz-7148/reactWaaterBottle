eb9849570968e99c1396bf682cbebb66efc6f781
mahfuz-7148 <mrahman7148@gmail.com>
2025-04-02 17:14:27 +0600

    added localstorage

--- src/components/Bottles.jsx
+++ src/components/Bottles.jsx
@@ -1,29 +1,58 @@
 import Cart from "./Cart.jsx";
 import Bottle from "./Bottle.jsx";
-import {use, useState} from "react";
+import {use, useEffect, useState} from "react";
+import {addToStoredCart, getStoreCart, removeFromCart} from "../utilities/localStorage.js";
 
 
 const Bottles = ({bottlesPromise}) => {
     const [cart, setCart] = useState([]);
     const bottles = use(bottlesPromise);
 
+    useEffect(() => {
+        const storedCartIds = getStoreCart();
+        // console.log(storedCartIds, bottles);
+
+        const storedCart = [];
+
+        for (const id of storedCartIds) {
+            // console.log(id);
+            const cartBottle = bottles.find(bottle => bottle.id === id);
+            if (cartBottle) {
+                storedCart.push(cartBottle);
+            }
+        }
+
+        console.log('stored cart', storedCart);
+        setCart(storedCart);
+
+    }, [bottles])
 
 
     const addToCart = bottle => {
         const newCart = [...cart, bottle]
         setCart(newCart)
+
+        addToStoredCart(bottle.id)
+    }
+
+    const handleRemoveFromCart = id => {
+        // console.log('remove item from the cart', id)
+
+        const remainingCart = cart.filter(bottle => bottle.id !== id);
+        setCart(remainingCart);
+        removeFromCart(id);
     }
 
     return (
         <div className='container mx-auto'>
             <h3 className='text-4xl text-center mt-5 mb-10'>Bottles: {bottles.length}</h3>
             <p>Added to cart: {cart.length}</p>
-            <Cart  cart={cart}/>
+            <Cart  cart={cart} handleRemoveFromCart={handleRemoveFromCart}/>
 
             <div className='grid grid-cols-4 gap-10'>
                 {
                     bottles.map(bottle =>
-                        <Bottle key={bottle.id}  bottle={bottle} addToCart={addToCart}/>)
+                        <Bottle key={bottle.id}  bottle={bottle} addToCart={addToCart} />)
                 }
             </div>
         </div>
--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,12 +1,12 @@
 
 
-const Cart = ({ cart }) => {
+const Cart = ({ cart, handleRemoveFromCart }) => {
     return (
         <div className='flex items-center gap-4 mb-5'>
             {
                 cart.map(bottle => <div key={bottle.id}>
                     <img className='w-20' src={bottle.img} alt="" />
-
+                    <button onClick={() => handleRemoveFromCart(bottle.id)}>x</button>
                 </div>)
             }
         </div>
